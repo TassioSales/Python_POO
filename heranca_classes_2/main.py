2a5ca4a3c10562a64a9b7ed0769337a1218a686e
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-04-09 13:07:38 +0100

    ajustes

---
+++ heranca_classes_2/main.py
@@ -0,0 +1,16 @@
+from classes import *
+
+c1 = Cliente("Tassio", 32)
+c1.comprar()
+c1.falar()
+
+print()
+
+a1 = Aluno("Maria", 54)
+a1.estudar()
+a1.falar()
+
+print()
+
+p1 = Pessoa("João", 25)
+p1.falar()
